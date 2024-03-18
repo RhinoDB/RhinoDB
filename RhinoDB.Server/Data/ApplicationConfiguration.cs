cb40d70c5a259936b747525067065f537125dcdd
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:19:43 -0400

    Add ApplicationConfiguration class
    A new class named ApplicationConfiguration has been added to the Server/Data directory of the project. This class uses JSON to map configuration settings including port, encryption key, log level and startup time.

---
+++ RhinoDB.Server/Data/ApplicationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Chase.CommonLib.FileSystem.Configuration;
+using Newtonsoft.Json;
+using Serilog.Events;
+
+namespace RhinoDB.Server.Data;
+
+public class ApplicationConfiguration : AppConfigBase<ApplicationConfiguration>
+{
+    [JsonProperty("port")] public int Port { get; set; } = 8080;
+
+    [JsonProperty("encryption-key")] public string EncryptionSalt { get; set; } = Guid.NewGuid().ToString("N");
+
+    [JsonProperty("log-level")] public LogEventLevel LogLevel { get; set; } = LogEventLevel.Information;
+
+    [JsonIgnore] public DateTime StartupTime { get; } = DateTime.Now;
+}
