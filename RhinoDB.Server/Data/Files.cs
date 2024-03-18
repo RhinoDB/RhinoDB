9e3c34aa73a17bc992643acc17642a45292f571b
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:18:55 -0400

    Add Files class for file path management
    A new static class called Files was added under RhinoDB.Server.Data namespace. This class contains properties which manage different file paths used within the application. Now, needed paths like ApplicationConfiguration, LatestLog, ErrorLog, and DebugLog are centrally managed within this Files class.

---
+++ RhinoDB.Server/Data/Files.cs
@@ -0,0 +1,11 @@
+﻿namespace RhinoDB.Server.Data;
+
+using static RhinoDB.Server.Data.Directories;
+
+public static class Files
+{
+    public static string ApplicationConfiguration { get; } = Path.Combine(Root, "application.json");
+    public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
+    public static string ErrorLog { get; } = Path.Combine(Logs, "error.log");
+    public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
+}
