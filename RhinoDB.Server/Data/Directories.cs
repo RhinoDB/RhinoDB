6d3e4ff9e4e353c94cf61e48ce87ea22a5a5ad2d
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:19:03 -0400

    Add Directories class to RhinoDB.Server.Data
    Introduced a new static class 'Directories' to the 'RhinoDB.Server.Data' namespace. This class stores the paths to the data root and log directories, creating them if they don't exist.

---
+++ RhinoDB.Server/Data/Directories.cs
@@ -0,0 +1,7 @@
+﻿namespace RhinoDB.Server.Data;
+
+public static class Directories
+{
+    public static string Root { get; } = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data")).FullName;
+    public static string Logs { get; } = Directory.CreateDirectory(Path.Combine(Root, "logs")).FullName;
+}
