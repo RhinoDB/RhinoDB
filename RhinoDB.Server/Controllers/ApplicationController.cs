111e003362b484bbfd58d7b4874bc9c6b9727768
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:18:13 -0400

    Add ApplicationController to handle API requests
    A new ApplicationController is introduced in this change to handle API requests. The controller includes a GET endpoint that returns application data. This is achieved by calling the GenerateApplicationData() method from the ApplicationData class.

---
+++ RhinoDB.Server/Controllers/ApplicationController.cs
@@ -0,0 +1,17 @@
+﻿// LFInteractive LLC. 2021-2024
+
+using Microsoft.AspNetCore.Mvc;
+using RhinoDB.Server.Data;
+
+namespace RhinoDB.Server.Controllers;
+
+[Route("/api")]
+[ApiController]
+public class ApplicationController : ControllerBase
+{
+    [HttpGet()]
+    public IActionResult Get()
+    {
+        return Ok(ApplicationData.GenerateApplicationData());
+    }
+}
