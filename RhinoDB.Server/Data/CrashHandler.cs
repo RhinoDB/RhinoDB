b5ee79c059478e966498561a34ade8852ad5ae71
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:19:11 -0400

    Add CrashHandler to handle and report exceptions
    A new class, CrashHandler, has been added which generates detailed crash reports, including the date/time, application name, OS, version, and crash data. It also makes sure to log the location of the crash report and expedites an orderly shutdown of the system after a crash.

---
+++ RhinoDB.Server/Data/CrashHandler.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+
+namespace RhinoDB.Server.Data;
+
+public static class CrashHandler
+{
+    public static void ReportAndExit(Exception ex)
+    {
+        string file = Report(ex);
+
+        Log.Error("A crash report has been generated at {file}", file);
+        Environment.Exit(1);
+    }
+
+    public static string Report(Exception ex)
+    {
+        DateTime now = DateTime.Now;
+        string formattedTime = now.ToString("dddd MMMM dd, yyyy - hh-mm-ss.fff tt");
+        string file = Path.Combine(Directories.Root, $"crash ({formattedTime}).txt");
+
+        using FileStream fs = new(file, FileMode.Create);
+        using StreamWriter writer = new(fs);
+        writer.WriteLine($"{ApplicationData.ApplicationName} Crash Report - {formattedTime}");
+        writer.WriteLine("\nApplication Data:");
+        writer.WriteLine($"\tOS: {Environment.OSVersion.VersionString}");
+        writer.WriteLine($"\tVersion: {ApplicationData.Version}");
+        writer.WriteLine("\nCrash Data:");
+        writer.WriteLine($"\tMessage: {ex.Message}");
+        writer.WriteLine($"\tSource: {ex.Source}");
+        writer.WriteLine($"\tData: {JsonConvert.SerializeObject(ex.Data)}");
+
+        writer.WriteLine($"Stack Trace:\n{ex.StackTrace}");
+
+        return file;
+    }
+}
