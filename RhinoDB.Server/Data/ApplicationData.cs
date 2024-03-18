ffe449a545babbd41ec104c0addf9d1d1f8e1135
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-18 18:19:26 -0400

    Add ApplicationData class in Server/Data
    A new class, ApplicationData, has been added under the Server/Data directory. This class provides static properties and a method for generating application-specific data, such as the application name, uptime, version and configuration.

---
+++ RhinoDB.Server/Data/ApplicationData.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace RhinoDB.Server.Data;
+
+public static class ApplicationData
+{
+    public static string ApplicationName { get; } = "RhinoDB.Server";
+    public static TimeSpan UpTime => DateTime.Now - ApplicationConfiguration.Instance.StartupTime;
+    public static Assembly MainAssembly { get; } = Assembly.GetExecutingAssembly();
+    public static AssemblyName? AssemblyName { get; } = MainAssembly.GetName();
+    public static Version? Version { get; } = AssemblyName.Version;
+
+    public static object GenerateApplicationData()
+    {
+        return new
+        {
+            ApplicationName,
+            Version,
+            UpTime,
+            ApplicationConfiguration.Instance.StartupTime,
+            Environment = "RELEASE",
+            Config = ApplicationConfiguration.Instance,
+        };
+    }
+}
